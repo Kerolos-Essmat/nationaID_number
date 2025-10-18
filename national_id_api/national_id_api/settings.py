6c9747a7dd0675de2980b6fc6e88834249c140f9
Kerolos Essmat <kerolosessmat8@gmail.com>
2025-10-18 18:05:13 +0300

    create app

--- national_id_api/national_id_api/settings.py
+++ national_id_api/national_id_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'national_id_info.apps.NationalIdInfoConfig',
 ]
 
 MIDDLEWARE = [
